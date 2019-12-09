e2aa04ce67c8967cb4e214db1dbfb6c81af8e3bc
serenadia <56970671+serenadia@users.noreply.github.com>
2019-12-08 20:55:10 -0500

    serena first commit
    poop

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+
+
+
+int main () {
+    
+    cout << "Welcome to Taco Bell! How may I take your order?" << endl;
+    cout << "Here's our menu: " << endl;
+    
+}
